7efaa78740bd6d65c1c1bb312563f7da8c2cdfd2
Silvia-CH <129101122+Silvia-CH@users.noreply.github.com>
2023-04-21 10:26:25 +0200

    Add files via upload

---
+++ PFC/vistas/libro/index.php
@@ -0,0 +1,47 @@
+<?php
+require_once('../database.php');
+$database = new Database();
+$tablaResul = $database->getAll('libro');
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>LIBRO</title>
+    <link rel='stylesheet' href='../style/style.css?ver=1.1'>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>ISBN</th>
+                <th>Titulo</th>
+                <th>Editorial</th>
+                <th>Sinopsis</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($tablaResul as $row) {
+                echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['isbn'] . "</td>";
+                echo "<td>" . $row['titulo'] . "</td>";
+                echo "<td>" . $row['editorial'] . "</td>";
+                echo "<td>" . $row['sinopsis'] . "</td>";
+                echo '<td> <a href="../delete.php?id=' . $row['id'] . '&table=libro">Eliminar</a></td>';
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
