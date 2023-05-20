26e41afc559f06e904c7c0704bfb2d07c6645f6a
NuriaLR <127306824+NuriaLR@users.noreply.github.com>
2023-05-21 00:55:09 +0200

    Create libroPersepolis.php

---
+++ LM/html/libroPersepolis.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/styleLibro.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="../img/BT.ico">
+    <title>Foro</title>
+</head>
+
+<body>
+    <nav>
+        <ul class="nav_izquierdo">
+            <a href="index.php">
+                <img id="icono" src="../img/logoV2.jpg">
+            </a>
+            <a href="contacto.php">
+                <li><i class="fas fa-envelope-open-text"></i> Contacto</li>
+            </a>
+            <li></li>
+        </ul>
+        <ul class="nav_centro">
+            <li class="buscador"><input type="search" placeholder="Buscar..."><i class="fas fa-search"></i></li>
+        </ul>
+        <ul class="nav_derecho">
+            <a href="foro.php">
+                <li>Foro</li>
+            </a>
+            <a href="login.php">
+                <li class="usuario"><i class="fas fa-user"></i> Iniciar Sesión</li>
+            </a>
+        </ul>
+    </nav>
+    <div class="mainNaside">
+        <main>
+            <section>
+                <div class="foto">
+                    <img src="../img/persepolis.png" width="300px" height="350px">                  
+                </div>
+                <div class="titulo">
+                    <h1>PERSÉPOLIS, Marjane Satrapi</h1>
+                </div>
+                <div class="sinopsis">
+                    <p>Persépolis nos cuenta la revolución islámica iraní vista desde los ojos de una niña que asiste atónita al cambio profundo que experimentan su país y su familia, mientras ella debe aprender a llevar el velo. Intensamente personal y profundamente político, el relato autobiográfico de Marjane Satrapi examina qué significa crecer en un ambiente de guerra y represión política.</p>
+                </div>
+            </section>
+        </main>
+    </div>
+    <footer>
+        <address>
+            <div>
+                <p>Si quieres una forma más directa de contactarnos:</p>
+                Email: hola@ejemplo.com
+                <br>
+                Teléfono: +34 777 77 77 77
+            </div>
+            <div>
+                <p>Conócenos en...</p>
+                <div class="redes">
+                    <a href="https://twitter.com"> <i class="fab fa-twitter"></i></a>
+                    <a href="https://telegram.org/"><i class="fab fa-telegram"></i></a>
+                    <a href="https://www.pinterest.com/"><i class="fab fa-pinterest"></i></a>
+                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </address>
+    </footer>
+</body>
+
+</html>
