875a7c28527198463def953fa45db2cd295cd637
NuriaLR <127306824+NuriaLR@users.noreply.github.com>
2023-05-22 23:33:28 +0200

    Create libroHistoriaDelArte.php

---
+++ LM/php/libroHistoriaDelArte.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/styleLibroHistoriaDelArte.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="../img/BT.ico">
+    <title>Historia del arte</title>
+</head>
+
+<body>
+    <nav>
+        <ul class="nav_izquierdo">
+            <a href="index.php">
+                <img id="icono" src="../img/logoV2.jpg">
+            </a>
+            <a href="contacto.php">
+                <li><i class="fas fa-envelope-open-text"></i> Contacto</li>
+            </a>
+            <li></li>
+        </ul>
+        <ul class="nav_centro">
+            <li class="buscador"><input type="search" placeholder="Buscar..."><i class="fas fa-search"></i></li>
+        </ul>
+        <ul class="nav_derecho">
+            <a href="foro.php">
+                <li>Foro</li>
+            </a>
+            <a href="login.php">
+                <li class="usuario"><i class="fas fa-user"></i> Iniciar Sesión</li>
+            </a>
+        </ul>
+    </nav>
+    <div>
+        <main>
+            <section id="main">
+                <div id="foto">
+                    <img src="../img/historiaArte.png" width="230px" height="350px">             
+                </div>
+                <div id="titulo">
+                    <h1>HISTORIA DEL ARTE, E.H Gombrich</h1>
+                </div>
+                <div id="sinopsis"> 
+                    <p>Es uno de los libros dedicados al arte más famosos y populares nunca escritos y ha sido 
+                       un best-seller durante medio siglo. Durante cinco décadas no ha tenido ningún rival como 
+                       introducción al arte en su totalidad, abarcando desde las pinturas rupestres primitivas 
+                       hasta el arte experimental contemporáneo.</p>                  
+                    <p>Si quieres ver un resumen de la historia del arte rápido y entretenido, visualiza este
+                    <a href="https://www.youtube.com/watch?v=rUHxLrZwSIY">Video</a></p>
+                    <p>Añade a tu lista:</p>
+                    <img class="favorito" src="../img/corazonlleno.png" width="30px" height="30px">   
+                </div>
+            </section>
+        </main>
+    </div>
+    <footer>
+        <address>
+            <div>
+                <p>Si quieres una forma más directa de contactarnos:</p>
+                Email: hola@ejemplo.com
+                <br>
+                Teléfono: +34 777 77 77 77
+            </div>
+            <div>
+                <p>Conócenos en...</p>
+                <div class="redes">
+                    <a href="https://twitter.com"> <i class="fab fa-twitter"></i></a>
+                    <a href="https://telegram.org/"><i class="fab fa-telegram"></i></a>
+                    <a href="https://www.pinterest.com/"><i class="fab fa-pinterest"></i></a>
+                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </address>
+    </footer>
+</body>
+
+</html>
