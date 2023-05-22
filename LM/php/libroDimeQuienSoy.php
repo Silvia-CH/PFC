63ed1d54211369cef7c7059e1c1284d311cb304b
NuriaLR <127306824+NuriaLR@users.noreply.github.com>
2023-05-22 23:14:10 +0200

    Create libroDimeQuienSoy.php

---
+++ LM/php/libroDimeQuienSoy.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/styleLibroDimeQuienSoy.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="../img/BT.ico">
+    <title>Dime quién soy</title>
+</head>
+
+<body>
+    <nav>
+        <ul class="nav_izquierdo">
+            <a href="index.php">
+                <img id="icono" src="../img/logoV2.jpg">
+            </a>
+            <a href="contacto.php">
+                <li><i class="fas fa-envelope-open-text"></i> Contacto</li>
+            </a>
+            <li></li>
+        </ul>
+        <ul class="nav_centro">
+            <li class="buscador"><input type="search" placeholder="Buscar..."><i class="fas fa-search"></i></li>
+        </ul>
+        <ul class="nav_derecho">
+            <a href="foro.php">
+                <li>Foro</li>
+            </a>
+            <a href="login.php">
+                <li class="usuario"><i class="fas fa-user"></i> Iniciar Sesión</li>
+            </a>
+        </ul>
+    </nav>
+    <div>
+        <main>
+            <section id="main">
+                <div id="foto">
+                    <img src="../img/dimeQuienSoy.png" width="250px" height="350px">             
+                </div>
+                <div id="titulo">
+                    <h1>DIME QUIÉN SOY Julia Navarro</h1>
+                </div>
+                <div id="sinopsis"> 
+                    <p>Un periodista investiga la apasionante vida de una antepasada suya, una mujer que vivió 
+                       intensamente el siglo XX desde los convulsos años de la República hasta la Caída del muro 
+                       de Berlín.</p>                  
+                    <p>Para saber más sobre esta novela gráfica, puedes ver la info de su
+                    <a href="https://www.filmaffinity.com/es/film476767.html">Serie</a></p>
+                    <p>Añade a tu lista:</p>
+                    <img class="favorito" src="../img/corazonlleno.png" width="30px" height="30px">   
+                </div>
+            </section>
+        </main>
+    </div>
+    <footer>
+        <address>
+            <div>
+                <p>Si quieres una forma más directa de contactarnos:</p>
+                Email: hola@ejemplo.com
+                <br>
+                Teléfono: +34 777 77 77 77
+            </div>
+            <div>
+                <p>Conócenos en...</p>
+                <div class="redes">
+                    <a href="https://twitter.com"> <i class="fab fa-twitter"></i></a>
+                    <a href="https://telegram.org/"><i class="fab fa-telegram"></i></a>
+                    <a href="https://www.pinterest.com/"><i class="fab fa-pinterest"></i></a>
+                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </address>
+    </footer>
+</body>
+
+</html>
