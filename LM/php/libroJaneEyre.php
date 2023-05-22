ca65130bdedcf73cb2868b22d466a1e01ddf7426
NuriaLR <127306824+NuriaLR@users.noreply.github.com>
2023-05-22 22:05:13 +0200

    Update libroJaneEyre.php

--- LM/php/libroJaneEyre.php
+++ LM/php/libroJaneEyre.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/styleLibroPersepolis.css">
+    <link rel="stylesheet" href="../style/styleLibroJaneEyre.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
     <link rel="icon" type="image/x-icon" href="../img/BT.ico">
     <title>Jane Eyre</title>
